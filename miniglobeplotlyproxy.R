6d8d7d339bd1c18076b64974281eb5c74068988f
wkumler <wkumler@uw.edu>
2024-12-18 15:02:01 -0800

    Adding robber demo and random location camera starts

--- miniglobeplotlyproxy.R
+++ miniglobeplotlyproxy.R
@@ -8,14 +8,32 @@ ui <- fillPage(
   includeCSS("styles.css")
 )
 
+camera_eye <- function() {
+  theta <- runif(1, 0, 2 * pi)
+  c(1.2 * cos(theta), 1.2 * sin(theta))
+}
+
 server <- function(input, output, session){
   observeEvent(input$add_point, {
     print("input$add_point clicked!")
-    newtrace <- list(x = list(0), y = list(0), z = list(15), key = list(0), 
-                     type = "scatter3d", mode = "markers", 
-                     marker = list(color = "red", opacity = 1, size = 50))
-    plotlyProxy("game_world") %>% 
-      plotlyProxyInvoke("addTraces", newtrace)
+    # newtrace <- list(x = list(0), y = list(0), z = list(15), key = list(0), 
+    #                  type = "scatter3d", mode = "markers", 
+    #                  marker = list(color = "red", opacity = 1, size = 50))
+    # plotlyProxy("game_world") %>% plotlyProxyInvoke("addTraces", newtrace)
+    
+    robber_init <- readRDS("game_files/QUNWYD/robber_data.rds")
+    newtrace <- list(
+      type="mesh3d",
+      x=as.list(robber_init$vertices$x),
+      y=as.list(robber_init$vertices$y),
+      z=as.list(robber_init$vertices$z),
+      i=as.list(robber_init$faces$i),
+      j=as.list(robber_init$faces$j),
+      k=as.list(robber_init$faces$k),
+      facecolor=as.list(robber_init$faces$color),
+      hoverinfo="none"
+    )
+    plotlyProxy("game_world") %>% plotlyProxyInvoke("addTraces", list(newtrace))
   })
   
   output$game_world <- renderPlotly({
@@ -24,6 +42,8 @@ server <- function(input, output, session){
     set_axis <- list(range=max(abs(globe_plates$vertices))*c(-1, 1),
                      autorange=FALSE, showspikes=FALSE,
                      showgrid=FALSE, zeroline=FALSE, visible=FALSE)
+    start_camera <- camera_eye()
+    print(start_camera)
     ply <- plot_ly(source = "game_world") %>%
       add_trace(type="mesh3d", data = globe_plates,
                 x=~vertices$x, y=~vertices$y, z=~vertices$z,
@@ -35,7 +55,7 @@ server <- function(input, output, session){
       layout(scene=list(
         xaxis=set_axis, yaxis=set_axis, zaxis=set_axis,
         aspectmode='cube',
-        camera=list(eye=list(x=0.8, y=0.8, z=0.8)),
+        camera=list(eye=list(x=start_camera[1], y=start_camera[2], z=0.8)),
         bgcolor="black"
       ),
       margin=list(l=0, r=0, b=0, t=0, pad=0),
